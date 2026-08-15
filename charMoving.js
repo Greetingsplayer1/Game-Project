b3a945653f8ca64985e6657f64875885dd630313
Sebastian <sebastianmseguin@gmail.com>
2026-08-15 12:25:36 -0400

    All the characters

--- charMoving.js
+++ charMoving.js
@@ -5,9 +5,34 @@ let chars = [
         desc: "Just a basic fighter. No specific strengths nor weaknesses. The most boring of the characters."
     },
     {
+        name: "Rogue",
+        src: "media/rogue.png",
+        desc: "A stealthy and dextrous outlaw who is quick at running and good with a bow."
+    },
+    {
+        name: "Knight",
+        src: "media/knight.png",
+        desc: "A slow but tanky soldier that has a very powerful sword."
+    },
+    {
         name: "Paladain",
         src: "media/paladain.svg",
         desc: "A tanky religious knight. Moves slowly but is great with a sword and has loads of health."
+    },
+    {
+        name: "Monk",
+        src: "media/monk.png",
+        desc: "A fighter that has trained long in martial arts. They are quick and dextrous."
+    },
+    {
+        name: "Ranger",
+        src: "media/ranger.png",
+        desc: "An able-bodied sharpshooter that is familiar with the wilderness. Has extra arrows and is poweful at using a bow."
+    },
+    {
+        name: "Bard",
+        src: "media/bard.png",
+        desc: "A musical spellcaster that is a jack of all trades."
     },
     {
         name: "Sorcerer",
